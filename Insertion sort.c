55998ae76992e69a1fed28972f118b05bc5c066e
Md. Ashraful Islam Talukder <ashraful.talukder24@gmail.com>
2021-11-24 08:26:50 +0600

    Create Insertion sort.c

---
+++ Insertion sort.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+void insertionSort(int arr[], int n)
+{
+    int i, j, temp;
+    for(i = 1; i < n; i++){
+    
+        temp = arr[i];
+        j = i - 1;
+    
+        while(j >= 0 && arr[j] > temp) {
+            arr[j+1] = arr[j];
+            
+            j = j-1;
+        }
+        
+        arr[j+1] = temp;
+    }
+}
+
+int main()
+{
+    int arr[] = {5, 6, 1, 2, 9};
+    int n = 5;
+    insertionSort(arr, n);
+    
+    for(int i = 0; i < n; i++){
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
